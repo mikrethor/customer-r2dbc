b00327393d77f024d207c91e5be85028b07ed8ad
mikrethor <mikrethor@gmail.com>
2019-05-07 21:37:27 -0400

    Correcting bug java 11 vs 8

--- src/main/kotlin/com/digitowl/r2dbc/config/Beans.kt
+++ src/main/kotlin/com/digitowl/r2dbc/config/Beans.kt
@@ -4,6 +4,7 @@ import com.digitowl.r2dbc.domain.Customer
 import com.digitowl.r2dbc.functionnal.CustomerHandler
 import com.digitowl.r2dbc.functionnal.router
 import com.digitowl.r2dbc.repository.CustomerRepository
+import com.digitowl.r2dbc.service.CustomerService
 import org.springframework.boot.CommandLineRunner
 import org.springframework.context.ApplicationContextInitializer
 import org.springframework.context.support.GenericApplicationContext
@@ -14,6 +15,7 @@ import reactor.core.publisher.Flux
 class Beans : ApplicationContextInitializer<GenericApplicationContext> {
     override fun initialize(context: GenericApplicationContext) = beans {
         bean { CustomerHandler(ref()) }
+        bean { CustomerService(ref()) }
         bean { router(ref()) }
         bean { init(ref()) }
     }.initialize(context)
@@ -21,9 +23,9 @@ class Beans : ApplicationContextInitializer<GenericApplicationContext> {
 
 internal fun init(repository: CustomerRepository) = CommandLineRunner {
     Flux.just(
-            Customer(null, "Cinnamon","Margetts"),
-            Customer(null, "Anthea","Scaddon"),
-            Customer(null, "Colene","Havenhand"))
+            Customer(null, "Cinnamon", "Margetts"),
+            Customer(null, "Anthea", "Scaddon"),
+            Customer(null, "Colene", "Havenhand"))
             .flatMap { repository.save(it) }
             .thenMany(repository.findAll())
             .subscribe { println(it) }
--- src/main/kotlin/com/digitowl/r2dbc/config/DbConfig.kt
+++ src/main/kotlin/com/digitowl/r2dbc/config/DbConfig.kt
@@ -1,13 +1,13 @@
 package com.digitowl.r2dbc.config
 
+import com.digitowl.r2dbc.repository.CustomerRepository
 import io.r2dbc.postgresql.PostgresqlConnectionConfiguration
 import io.r2dbc.postgresql.PostgresqlConnectionFactory
 import io.r2dbc.spi.ConnectionFactory
-import com.digitowl.r2dbc.repository.CustomerRepository
-import de.flapdoodle.embed.process.runtime.Network
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.DependsOn
 import org.springframework.data.r2dbc.config.AbstractR2dbcConfiguration
 import org.springframework.data.r2dbc.function.DatabaseClient
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories
@@ -73,6 +73,7 @@ class DbConfig : AbstractR2dbcConfiguration() {
         return DatabaseClient.create(connectionFactory)
     }
 
+    @DependsOn("postgresProcess")
     @Bean
     override fun connectionFactory(): ConnectionFactory {
         return PostgresqlConnectionFactory(
